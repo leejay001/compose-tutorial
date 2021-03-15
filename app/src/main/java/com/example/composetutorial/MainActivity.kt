605f68787a2b2688b387d1f0e3874165234d932a
lishuaipu <1090266148@qq.com>
2021-03-15 17:53:06 +0800

    fix: update latest compose version and unfollowed the tutorial caseof func was deleted

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,36 +3,73 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+//            PreviewGreeting()
+            DefaultPreview()
         }
     }
 }
 
+
+//@Composable
+//fun Greeting(name: String){
+//    Text(text = "Hello $name")
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewGreeting(){
+//    Greeting("Android")
+//}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Text("A day in Shark Fin Cove")
+    Text("Davenport, California")
+    Text("December 2018")
 }
 
-@Preview(showBackground = true)
+// version=1.0.0-beta02,use painterResource instead of imageResource which has been deleted.
+//https://developer.android.com/reference/kotlin/androidx/compose/ui/res/package-summary#painterresource
+@Composable
+fun NewStory() {
+    val img = painterResource(R.drawable.header)
+
+    Column(
+        modifier = Modifier.padding(20.dp)
+    ) {
+        val imageModifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth()
+
+        Image(img, null, imageModifier,contentScale = ContentScale.Crop)
+
+        Spacer(Modifier.height(16.dp))
+
+        Text("This is first")
+        Text("This is second")
+        Text("This is third")
+    }
+}
+
+@Preview
 @Composable
 fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
+    NewStory()
 }

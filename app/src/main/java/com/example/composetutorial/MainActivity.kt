35726d58e74b9a731fe358a486662cb08a7bee00
lishuaipu <1090266148@qq.com>
2021-03-15 18:03:11 +0800

    feat: material design img and text“

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
@@ -51,21 +54,36 @@ fun NewsStory() {
 fun NewStory() {
     val img = painterResource(R.drawable.header)
 
-    Column(
-        modifier = Modifier.padding(20.dp)
-    ) {
-        val imageModifier = Modifier
-            .height(180.dp)
-            .fillMaxWidth()
+    MaterialTheme() {
+        val typography = MaterialTheme.typography
 
-        Image(img, null, imageModifier,contentScale = ContentScale.Crop)
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            val imageModifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(8.dp))
 
-        Spacer(Modifier.height(16.dp))
+            Image(img, null, imageModifier,contentScale = ContentScale.Crop)
 
-        Text("This is first")
-        Text("This is second")
-        Text("This is third")
+            Spacer(Modifier.height(16.dp))
+
+            Text( "A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+            style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis)
+
+            Text("This is second",
+            style = typography.body2)
+            Text("This is third",
+            style = typography.body2)
+        }
     }
+
+
 }
 
 @Preview
